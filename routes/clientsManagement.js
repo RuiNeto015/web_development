aa3074bf0e4420db5616f3d5bb83950eef55547c
RuiNeto015 <ruineto015@gmail.com>
2022-04-19 23:42:23 +0100

    navbar

--- routes/clientsManagement.js
+++ routes/clientsManagement.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET login page. */
+/* GET clients page. */
 router.get('/', function(req, res, next) {
-  res.render('login', { title: 'Express' });
+  res.render('clientsManagement');
 });
 
 module.exports = router;
