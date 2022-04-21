9c8ee6c1443f7700ffc3fe26219bcdecf708fa46
MrSparkiee <diogo.2002.miguel@gmail.com>
2022-04-21 13:26:45 +0100

    Layouts

--- routes/login.js
+++ routes/login.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET login page. */
 router.get('/', function(req, res, next) {
-  res.render('login');
+  res.render('login', { title: 'Login' });
 });
 
 module.exports = router;
