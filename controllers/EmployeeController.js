8c3e5cf0df20518948c5d97c4ea3c7fba1ca51f3
RuiNeto015 <ruineto015@gmail.com>
2022-04-26 15:06:00 +0100

    some

--- controllers/EmployeeController.js
+++ controllers/EmployeeController.js
@@ -1,19 +1,12 @@
 var mongoose = require("mongoose");
 var Employee = require("../models/EmployeeModel");
 
-var EmployeeController = {};
-
 const addEmployee = function(req, res){
     var employee = Employee(req.body);
 
-    employee.save((err) => {
-        if(err){
-            console.log(err);
-            res.render("../views/employees/create");
-        } else{
-            console.log("Successfully created an employee.");
-            res.redirect("/employees");
-        }
+    employee.save(() => {
+        console.log("Successfully created an employee.");
+        res.render('../views/employees', {title:"Funcionários"});
     })
 }
 
