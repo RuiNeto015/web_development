2e167079ef109252fb718f7bfa022d207922a57f
RuiNeto015 <ruineto015@gmail.com>
2022-04-28 20:44:21 +0100

    Edit books

--- controllers/BookController.js
+++ controllers/BookController.js
@@ -12,7 +12,6 @@ const getAllBooks = function(req, res){
 
 const addBook = function(req, res){
     var book = Book(req.body);
-
     book.save((err) => {
         if(err){res.status(400)}
         console.log("Successfully created a book.");
@@ -20,7 +19,50 @@ const addBook = function(req, res){
     })
 }
 
+const deleteBook = function(req, res){
+    Book.remove({_id: req.params.id}, function(err){
+        if(err){res.status(400)}
+        console.log(err);
+        console.log("Successfully deleted a book.");
+        res.redirect('/books');
+    })
+}
+
+const getBookEditPage = function(req, res){
+    Book.findOne({_id: req.params.id}).exec(function(err, book){
+        if(err){res.status(400)}
+        res.render('books/edit', {
+            book: book,
+            title: "Livros"
+        })
+    });
+}
+
+const updateBook = function(req, res){
+    Book.findByIdAndUpdate(req.params.id, {
+        $set: {
+            title: req.body.title,
+            author: req.body.author,
+            isbn: req.body.isbn,
+            editor: req.body.editor,
+            language: req.body.language,
+            quantity: req.body.quantity,
+            condition: req.body.condition,
+            pages: req.body.pages
+        }
+    }, {new: true}, function(err, book){
+        if(err){
+            console.log(err);
+            res.render('../views/books/edit', {book: req.body});
+        }
+        res.redirect("/books")
+    });
+}
+
 module.exports = {
     getAllBooks,
-    addBook
+    addBook,
+    deleteBook,
+    getBookEditPage,
+    updateBook
 }
--- routes/books.js
+++ routes/books.js
@@ -1,11 +1,13 @@
 var express = require('express');
+const req = require('express/lib/request');
 var router = express.Router();
-const {getAllBooks,addBook} = require("../controllers/BookController");
+const {getAllBooks,addBook, deleteBook, getBookEditPage, updateBook} = require("../controllers/BookController");
 
 //BOOKS INDEX
 
 /* GET Books Index page. */
 router.get('/books', getAllBooks);
+
 //BOOKS CREATE
 
 /* GET Books Create page. */
@@ -14,6 +16,16 @@ router.get('/books/create', function(req, res, next) {
 });
 
 router.post('/books/create', addBook);
+
+//BOOKS DELETE
+
+router.delete('/books/delete/:id', deleteBook);
+
+//BOOKS EDIT
+
+router.get('/books/edit/:id', getBookEditPage);
+
+router.post('/books/edit/:id', updateBook);
 
 module.exports = {
   routes: router
