131dab3c5a08459b16ef87678f08a59a841261fc
MrSparkiee <diogo.2002.miguel@gmail.com>
2022-05-06 15:23:52 +0100

    fixes

--- models/PurchaseModel.js
+++ models/PurchaseModel.js
@@ -3,33 +3,47 @@ var mongoose = require("mongoose");
 var PurchaseSchema = new mongoose.Schema({
   nif: {
     type: Number,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
   },
   name: {
     type: String,
+    required: true,
   },
   email: {
     type: String,
+    required: true,
   },
   phoneNumber: {
     type: Number,
+    required: true,
   },
   address: {
     type: String,
+    required: true,
   },
   isbn: {
     type: [Number],
+    required: true,
   },
   title: {
     type: [String],
+    required: true,
   },
   condition: {
     type: [String],
+    required: true,
   },
   quantity: {
     type: [Number],
+    required: true,
   },
   price: {
     type: [String],
+    required: true,
   },
   created_at: {
     type: Date,
