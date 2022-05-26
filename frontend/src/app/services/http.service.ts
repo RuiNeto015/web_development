68c3ca8f49d4655b42fb96b03f70190918894d49
MrSparkiee <diogo.2002.miguel@gmail.com>
2022-05-26 14:28:40 +0100

    Update http.service.ts

--- frontend/src/app/services/http.service.ts
+++ frontend/src/app/services/http.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Book } from '../Models/Book';
+import { delay, Observable, retry, retryWhen, scan } from 'rxjs';
 
 const API_DOMAIN = 'http://localhost:3000/';
 const GOOGLE_API = 'https://www.googleapis.com/books/v1/volumes?q=isbn:';
@@ -16,10 +17,21 @@ export class HttpService {
 
   getBooksCover(books: any) {
     books.forEach((book: any) => {
-      this.http.get(GOOGLE_API + book.isbn).subscribe((data: any) => {
+      this.http.get(GOOGLE_API + book.isbn).pipe(this.handleRetry).subscribe((data: any) => {
         book.cover = data.items[0].volumeInfo.imageLinks.thumbnail;
       });
     });
     return books;
   }
+
+  private handleRetry<T>(source: Observable<T>): Observable<T> {
+    return source.pipe(retryWhen(e => e.pipe(scan((errorCount, error) => {
+        if (errorCount >= 3) {
+            throw error;
+        }
+        return errorCount + 1;
+    }, 0),
+        delay(1000)
+    )));
+}
 }
