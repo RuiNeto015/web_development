c324aaec9b42d41bb95998c528f7b725d69b0b33
RuiNeto015 <ruineto015@gmail.com>
2022-06-02 23:35:27 +0100

    keep

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,26 +3,21 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { BookDetailsComponent } from './book-details/book-details.component';
 import { BooksListComponent } from './books-list/books-list.component';
+import { AuthGuardService } from './services/auth-guard.service';
 import { UserProfileComponent } from './user-profile/user-profile.component';
+//import { LoginComponent } from './login';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: BooksListComponent,
-  },
-  {
-    path: 'search/:search-by/:book-search',
-    component: BooksListComponent,
-  },
-  {
-    path: 'bookDetails/:isbn',
-    component: BookDetailsComponent,
-  },
-  {
-    path: 'profile',
-    component: UserProfileComponent,
-  },
-];
+  { path: '', component: BooksListComponent },
+  { path: 'search/:search-by/:book-search', component: BooksListComponent },
+  { path: 'bookDetails/:isbn', component: BookDetailsComponent, canActivate: [AuthGuardService] },
+  { path: 'profile', component: UserProfileComponent },
+  { path: '', component: BooksListComponent, canActivate: [AuthGuardService] },
+  //{ path: 'login', component: LoginComponent },
+  { path: '**', redirectTo: '' }
+]; 
+
+export const routing = RouterModule.forRoot(routes);
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
